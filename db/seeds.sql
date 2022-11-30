c2820e512cc26d7ca6b4df8422b95d3b71462419
unknown <phamexb@gmail.com>
2022-11-30 12:49:25 -0800

    added in query and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (department_name)
+VALUES ("Marketing"), ("Accounting"), ("Engineering"),("Legal"), ("Sales")
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Salesman", 60000, 5), ("Lawyer", 210000, 5), ("Front-end Engineer", 130000, 3), ("Accountant", 80000, 2), ("Marketer", 760000, 1), ("Lead Engineer", 400000, 3)
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Bob", "Johnson", 1),("Randy", "Johnson", 2), ("Bob", "Scott", 3),("Cindy", "Barton", 6), ("Chris", "Taylor", 3, 4)
